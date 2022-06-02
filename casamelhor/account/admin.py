f3811dc23d2d8a34e0b7ce0aa3b8f71d4ac20997
Lokesh sahu <sles.shuban@gmail.com>
2022-06-02 12:21:05 +0530

    account modify

--- casamelhor/account/admin.py
+++ casamelhor/account/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Permission
 from .models import *
-from .forms import RegisterForm, AdminForm
+from .forms import AdminForm
 
 
 class RoleAdmin(admin.ModelAdmin):
