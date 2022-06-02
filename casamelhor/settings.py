c71b5554605d4fd8eb6416d8865795c34258ba55
Lokesh sahu <sles.shuban@gmail.com>
2022-06-02 11:36:43 +0530

    user model customized

--- casamelhor/settings.py
+++ casamelhor/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7y3@ul_h@am97^hx^i8^=+t=i^t&u6siw9a6i1_3l2h2-9hfut'
+s = os.environ.get('SECRET_KEY')
+# s = 'django-insecure-7y3@ul_h@am97^hx^i8^=+t=i^t&u6siw9a6i1_3l2h2-9hfut'
+SECRET_KEY = s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -114,7 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 # Default primary key field type
@@ -124,3 +127,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
